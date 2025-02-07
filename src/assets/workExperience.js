680ec1e8643746ebff881be9e90802bfa48d098b
acooma <acoozee@gmail.com>
2025-02-07 08:16:16 -0600

    grammar

--- src/assets/workExperience.js
+++ src/assets/workExperience.js
@@ -1,7 +1,7 @@
 export const workExperience = [
   {
     name: "Discover Financial Services",
-    description: "Developed applications for Discover Card & Bank, including solutions for Student Loans, Personal Loans, and Deposits products, streamlining tasks such as payments and transactions. Additionally, constructed an automated test suite, a reusable component library, a CLI tool, and various applications using React and Spring Boot, contributing to operational efficiency and user experience improvement.",
+    description: "Developed applications for Discover Card & Bank, including solutions for Student Loans, Personal Loans, and Deposits products, streamlining tasks such as payments and transactions. Additionally, constructed an automated test suite, a reusable component library, a CLI tool, and various applications using React and Spring Boot, enhancing operational efficiency and user experience.",
     date: "July 2020",
     isCurrent: true,
     logo: "👔",
