b542eb0111c5af6f5bc153b6ddc219da5d13f1fa
Angelo Cruz <angelo.t.cruz1@gmail.com>
2025-07-09 13:10:30 -0500

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')).render(
-  // <>
-  //   <App />
-  // </>
-  <div className="relative w-full min-h-screen">
-    {/* <div className="relative z-10 flex items-center justify-center h-full"> */}
-    {/* </div> */}
-      <App />
-  </div>,
+  <>
+    <App />
+  </>
 )
